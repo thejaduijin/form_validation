42a03abcdfadd2cc37c3590f2cb8d3f301949a41
Lokendra Singh <lokendra.06.work@gmail.com>
2025-01-20 15:41:01 +0530

    form created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './App.css'
 import LoginForm from './components/LoginForm'
+import SignupForm from './components/SignupForm'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-blue-700">
-        Login
-      </h1>
-
-      <LoginForm></LoginForm>
+      <SignupForm></SignupForm>
+      {/* <LoginForm></LoginForm> */}
     </>
   )
 }
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-
-function LoginForm() {
-    const [formData, setFormData] = useState({ email: "", password: "" });
+function SignupForm() {
+    const [formData, setFormData] = useState({ name: "", email: "", password: "" });
     const [error, setError] = useState("");
 
     const handleChange = (e: any) => {
@@ -12,20 +11,31 @@ function LoginForm() {
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
-        if (!formData.email || !formData.password) {
-            setError("Both fields are required.");
+        if (!formData.name || !formData.email || !formData.password) {
+            setError("All fields are required.");
             return;
         }
         setError("");
         console.log("Form Submitted:", formData);
-        setFormData({ email: "", password: "" });
+        setFormData({ name: "", email: "", password: "" });
     }
 
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="bg-gray-200 p-6 rounded-xl w-96 gap-2">
-                <h1 className="text-3xl font-bold text-blue-700 flex justify-center items-center">Login</h1>
+                <h1 className="text-3xl font-bold text-blue-700 flex justify-center items-center">Signup</h1>
                 <form onSubmit={handleSubmit} className="flex justify-center items-center flex-col">
+                    <div className="mt-4">
+                        <input
+                            className="p-4 rounded"
+                            type="text"
+                            placeholder="Name"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </div>
                     <div className="mt-4">
                         <input
                             className="p-4 rounded"
@@ -50,11 +60,11 @@ function LoginForm() {
                     </div>
 
                     {error && <p className="text-red-700">{error}</p>}
-                    <button className="bg-emerald-600 rounded text-xl px-4 py-2 font-semibold mt-4" type="submit">Login</button>
+                    <button className="bg-emerald-600 rounded text-xl px-4 py-2 font-semibold mt-4" type="submit">Signup</button>
                 </form>
             </div>
         </div>
     )
 }
 
-export default LoginForm
+export default SignupForm

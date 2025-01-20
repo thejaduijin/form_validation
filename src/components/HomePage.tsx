42220cbe7f8d64c96b77cccbd1e811a10bcedcd5
Lokendra Singh <lokendra.06.work@gamil.com>
2025-01-20 22:15:30 +0530

    tricky added

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import LoginForm from "./LoginForm";
+import Tricky from "./Tricky";
 
 function HomePage() {
   const [isLoggedOut, setIsLoggedOut] = useState(false);
@@ -14,8 +15,13 @@ function HomePage() {
       {
         isLoggedOut ?
           <LoginForm /> :
-          <div className="flex justify-end items-end mr-4">
-            <button className="bg-red-600 rounded text-xl px-4 py-2 font-semibold mt-4" onClick={handleLogout}>Logout</button>
+          <div>
+            <div className="flex justify-end items-end mr-4">
+              <button className="bg-red-600 rounded text-xl px-4 py-2 font-semibold mt-4" onClick={handleLogout}>Logout</button>
+            </div>
+            <div>
+              {/* <Tricky></Tricky> */}
+            </div>
           </div>
       }
 
